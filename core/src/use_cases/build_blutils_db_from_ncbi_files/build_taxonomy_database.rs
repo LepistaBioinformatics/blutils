52ad8fdf2e4c255c738e89bd3f2313e2d65cbe03
Samuel Galvão Elias <sgelias@outlook.com>
2024-03-14 14:03:31 -0300

    refactor: renament blutils database generator use-case
    main

--- core/src/use_cases/build_blutils_db_from_ncbi_files/build_taxonomy_database.rs
+++ core/src/use_cases/build_blutils_db_from_ncbi_files/build_taxonomy_database.rs
@@ -46,7 +46,7 @@ pub(crate) struct RankedTaxidUnit {
         drop_non_linnaean_taxonomies,
     )
 )]
-pub(super) fn build_taxonomy_database(
+pub(build_blutils_db_from_ncbi_files) fn build_taxonomy_database(
     names_path: PathBuf,
     nodes_path: PathBuf,
     lineage_path: PathBuf,
--- core/src/use_cases/mod.rs
+++ core/src/use_cases/mod.rs
@@ -1,15 +1,17 @@
+mod build_blutils_db_from_ncbi_files;
 mod build_consensus_identities;
+mod build_kraken_db_from_ncbi_files;
 mod build_qiime_db_from_blutils_db;
-mod build_ref_db_from_ncbi_files;
 mod check_host_requirements;
 mod parse_consensus_as_tabular;
 mod run_blast_and_build_consensus;
 mod shared;
 mod write_blutils_output;
 
+pub use build_blutils_db_from_ncbi_files::*;
 pub use build_consensus_identities::*;
+pub use build_kraken_db_from_ncbi_files::*;
 pub use build_qiime_db_from_blutils_db::*;
-pub use build_ref_db_from_ncbi_files::*;
 pub use check_host_requirements::*;
 pub use parse_consensus_as_tabular::*;
 pub use run_blast_and_build_consensus::*;
